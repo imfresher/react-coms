4ce6343c68b338abcf380957ca56e9eb1a6de029
mr-kevinduy <mr.kevinduy@gmail.com>
2022-11-05 18:58:58 +0700

    Add and change to Typescript

---
+++ src/components/table/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Table() {
+  return (
+    <table>
+      <thead>
+
+      </thead>
+
+      <tbody>
+
+      </tbody>
+    </table>
+  );
+}
+
+export default Table;
